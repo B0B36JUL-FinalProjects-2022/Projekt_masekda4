edef0a076c7fbeb9dfcbd1487a730118e21bedad
David Mašek <davidmasek42@gmail.com>
2022-11-06 13:18:40 +0100

    add visualization utility

---
+++ src/visualize.jl
@@ -0,0 +1,39 @@
+function sprint_action(env::SnakeGame, action::Integer)::String
+    action = env.ACTION_SPACE[action]
+    if action == UP
+        return "↑"
+    elseif action == DOWN
+        return "↓"
+    elseif action == LEFT
+        return "←"
+    elseif action == RIGHT
+        return "→"
+    else
+        return "<unexpected>"
+    end
+end
+
+
+function display_game(env, agent; max_steps=nothing)
+    "Episode return (total reward)"
+    G = 0
+    state = reset!(env)
+    println("Score: $G")
+    show(env)
+    i = 1
+    while true
+        action = choose_action(agent, state)
+        println("Action: $(sprint_action(env, action))")
+        next_state, reward, done = step!(env, action)
+        G += reward
+
+        println("Score: $G")
+        show(env)
+
+        if done || (max_steps !== nothing && i > max_steps)
+            break
+        end
+        state = next_state
+        i += 1
+    end
+end
